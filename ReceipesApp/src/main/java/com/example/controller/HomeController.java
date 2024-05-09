53d2f087c237adfae5870da42d67b77547454ad4
Tanay Banerjee <41425062+Tanayban@users.noreply.github.com>
2024-05-09 21:32:25 +0530

    Update

--- ReceipesApp/src/main/java/com/example/controller/HomeController.java
+++ ReceipesApp/src/main/java/com/example/controller/HomeController.java
@@ -25,6 +25,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,7 @@ import com.example.config.ProjectConfig;
 
 
 @Controller
+@CrossOrigin
 public class HomeController {
 	
 	@Autowired
